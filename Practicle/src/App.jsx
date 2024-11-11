1f322330d0d986ae71dd780ff34a0d237912c544
ibrahim <ibrahimburhanpurwala84@gmail.com>
2024-11-11 11:42:54 +0530

    add

---
+++ Practicle/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import IncrementDecrement from './IncrementDecrement'
+import Home from './Home'
+import Login from './Login'
+import Logout from './Logout'
+
+
+export default function App() {
+  return (
+    <>
+      <center>
+        {/* <IncrementDecrement></IncrementDecrement> */}
+        {/* <Home></Home> */}
+        {/* <Login></Login> */}
+        {/* <Logout></Logout> */}
+      </center>
+    </>
+  )
+}
---
+++ Practicle/src/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Login.css"
+
+export default function Login() {
+    return (<>
+
+        <div className='mainbox'>
+
+            <div className='navigation'>
+                <button className=''>login</button>
+            </div>
+
+            <div className='middle'>
+                <div className='text'>
+                    <p>Public Views</p>
+                </div>
+            </div>
+            <div className='footer'>
+                <div className='text1'>
+
+                    <p>Footer</p>
+                </div>
+
+            </div>
+        </div>
+    </>
+
+    )
+}
