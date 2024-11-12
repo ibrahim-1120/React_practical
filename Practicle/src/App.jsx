35d3340be273995a17f3a2432d03de30b260b5c8
ibrahim <ibrahimburhanpurwala84@gmail.com>
2024-11-12 16:30:45 +0530

    add

--- Practicle/src/App.jsx
+++ Practicle/src/App.jsx
@@ -14,6 +14,10 @@ export default function App() {
         {/* <Login></Login> */}
         {/* <Logout></Logout> */}
       </center>
+
+
+
+
     </>
   )
 }
---
+++ Webpage/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Index from './webpage'
+
+export default function App() {
+  return (
+    <div>
+      <Index></Index>
+    </div>
+  )
+}
